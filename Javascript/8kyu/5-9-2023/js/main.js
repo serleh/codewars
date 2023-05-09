8f3699a335b3897fc4df0f7a8f4235de16fe083b
Saleh Abdullahi <salehabdullahi018@gmail.com>
2023-05-09 15:35:16 +0100

    DOne

---
+++ Javascript/8kyu/5-9-2023/js/main.js
@@ -0,0 +1,24 @@
+// Make a function that will return a greeting statement that uses an input; your program should return,
+// "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// SOLUTION
+
+// P - input
+// R - string
+// E -  
+// P - 
+
+// function greeting(name){
+
+//   return `Hello, ${name} how are you doing today?`
+// }
+
+let greeting = name => `Hello, ${name} how are you doing today?`
+
+console.log(greeting('Saleh'))
+
+
+
+
