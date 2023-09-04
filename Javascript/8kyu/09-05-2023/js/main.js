38958139867216b54f3b72b035cfd9f80352a0f8
Saleh Abdullahi <salehabdullahi018@gmail.com>
2023-09-05 00:44:11 +0100

    solved division problem (8kyu)

---
+++ Javascript/8kyu/09-05-2023/js/main.js
@@ -0,0 +1,6 @@
+//This method, which is supposed to return the result of dividing its first argument
+ //by its second, isn't always returning correct values. Fix it.
+
+ const solve = (x, y) => x / y
+
+ console.log(solve(2,1))
