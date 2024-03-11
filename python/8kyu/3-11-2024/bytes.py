cabc86959d6e31c74fc3d1e582b8666d12703f92
Saleh Abdullahi <salehabdullahi018@gmail.com>
2024-03-11 15:57:55 +0100

    Solved sys byte size challenge

---
+++ python/8kyu/3-11-2024/bytes.py
@@ -0,0 +1,12 @@
+# Task:
+
+# In this kata, you need to return the number of bytes (aka. memory size) a given object takes up.
+# Different variable types will be given, but they will all be valid.
+# Also, random generation for strings takes out of Unicode, not the regular 255 ascii letters.
+
+import sys
+
+def total_bytes(object):
+    return sys.getsizeof(object)
+
+print(total_bytes('Hello'))
