98a792ea9595a8b033ea3fdf48f3a92a64e6093c
Saleh Abdullahi <salehabdullahi018@gmail.com>
2024-04-26 09:14:50 +0100

    solved flick-switch after alot of trials and error 🤪

---
+++ python/8kyu/4-26-2024/flick_switch.py
@@ -0,0 +1,24 @@
+# Task
+# Create a function that always returns True/true for every item in a given list.
+# However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+# Examples
+# ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+# ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+# ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+
+def flick_switch(lst):
+    list=[]
+    is_flick=True
+
+    for i in lst:
+        if i =='flick':
+            list.append(not is_flick)
+            is_flick = not is_flick
+        else:
+            list.append(is_flick)
+    return list
+
+print(flick_switch(['bicycle', 'jarmony', 'flick', 'sheep', 'flick']))
