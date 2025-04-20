d45ef5b70b64d19b0d97731238b9d3cccd2f065c
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-04-20 20:47:30 +0100

    Solved better than average challenge

---
+++ Javascript/8kyu/04-20-25/index.js
@@ -0,0 +1,18 @@
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+// Note:
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+function betterThanAverage(classpoints, yourpoints) {
+  return (
+    yourpoints > classpoints.reduce((acc, c) => acc + c, 0) / classpoints.length
+  );
+}
+
+console.log(betterThanAverage([10, 20, 30, 55, 60], 55));
