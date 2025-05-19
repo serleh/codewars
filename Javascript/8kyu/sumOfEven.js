0985589b70be3cae2e692509131c355e61ae1e18
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-05-19 23:29:42 +0100

    Solved evenChar challenge

---
+++ Javascript/8kyu/sumOfEven.js
@@ -0,0 +1,28 @@
+// PREP
+// A function that returns a sequence(indexstarts at 1) of all the even characters of a string
+// if String is smaller than two characters or longer than 100 characters, it should return 'invalid string'
+
+// P -> String
+// R -> Array of even numbers
+// E
+
+function evenChars(string) {
+  if (typeof string !== "string" || string.length < 2 || string.length > 100) {
+    return "invalid string";
+  }
+  let result = "";
+  for (let i = 1; i < string.length; i += 2) {
+    result += string[i];
+  }
+  result = result.split("");
+  return result;
+}
+
+// function evenChars(string) {
+//   return (string.length < 2 || string.length > 100) ? "invalid string" :
+//   [...string].filter((x, i) => i % 2);
+// }
+
+// E
+console.log(evenChars("abcdefghijklm")); // -> ["b", "d", "f", "h", "j", "l"]
+console.log(evenChars("1234")); // -> "invalid string"
