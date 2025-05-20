1d9ff576a8e0a79a371623f4bb4ac27a4f1f76e7
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-05-20 23:48:37 +0100

    Solved word search problem

---
+++ Javascript/7kyu/array-search.js
@@ -0,0 +1,18 @@
+//PREP
+// A method that will search an array of strings that contains another string, ignoring capitalization.
+// Then return an array of the found string
+
+// P --> query string, and array to search
+//R -->  array
+// E
+
+function wordSearch(query, seq) {
+  const result = seq.filter((word) =>
+    word.toLowerCase().includes(query.toLowerCase())
+  );
+
+  return result.length > 0 ? result : ["Empty"];
+}
+
+console.log(wordSearch("me", ["home", "milk", "Mercury", "fish"])); // ->['home''Mercury]
+console.log(wordSearch("him", ["home", "milk", "Mercury", "fish"])); // ->['Empty']
