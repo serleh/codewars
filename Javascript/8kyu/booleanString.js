f6544921bbca9e386248146f033f57dc0e94c919
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-07-04 09:07:09 +0100

    Solved Bool 2 String

---
+++ Javascript/8kyu/booleanString.js
@@ -0,0 +1,17 @@
+//PREP
+// A function which convert the given boolean to  a string
+
+// P ---> Boolean
+//R ----> String
+
+function booleanString(b) {
+  // P
+  // accept a boolean
+  // turns it to string
+
+  return String(b);
+}
+
+//E
+console.log(booleanString(true)); // ---> 'true'
+console.log(booleanString(false)); // ---> 'false'
